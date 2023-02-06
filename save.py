0286dcb3b902a0099943c5c1f86cfd3e14cb701a
Dobinda-Alessandro <dobinda.alessandro@iisgalvanimi.edu.it>
2023-02-06 18:32:40 +0000

    1

---
+++ save.py
@@ -0,0 +1,4 @@
+#<tr><td><li><a href="#cibo"> Che cos'è il cibo</a></li></td> </tr>
+<tr><td><li><a href="#piramide"> La piramide alimentare</a></li></td> </tr>
+<tr><td><li><a href="#biomolecole">Le biomolecole</a></li> </td> </tr>
+<tr><td><li><a href="#io">I miei cibi preferiti</a></li> </td></tr>
